416db5ddfbff3106279d984e7511963d18cfca19
ivanzheng <ivannzheng@gmail.com>
2024-11-18 22:28:12 -0500

    mini changes to dectection range

--- main.py
+++ main.py
@@ -17,31 +17,31 @@ colors = {
 
 color_hsv = colors[color_input]
 
-# Initialize video capture
+
 cap = cv2.VideoCapture(0)
 
-# Create background subtractorbl, to not pick up anything in the background
+
 fgbg = cv2.createBackgroundSubtractorMOG2(history=500, varThreshold=25, detectShadows=True)
 
 while True:
     ret, frame = cap.read()
 
-    # Apply background subtractor to get the foreground mask
+    
     fgmask = fgbg.apply(frame)
 
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
     fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
 
-    # Apply the foreground mask to the original frame to isolate the foreground
+    
     foreground = cv2.bitwise_and(frame, frame, mask=fgmask)
 
-    # Convert the foreground to HSV
+  
     hsvImage = cv2.cvtColor(foreground, cv2.COLOR_BGR2HSV)
 
-    # Get HSV limits for the specified color
+    
     limits = get_limits(color=color_hsv)
 
-    # Create a mask for the specified color in the foreground
+  
     if color_input == "red":
         mask1 = cv2.inRange(hsvImage, limits[0][0], limits[0][1])
         mask2 = cv2.inRange(hsvImage, limits[1][0], limits[1][1])
@@ -64,10 +64,7 @@ while True:
         if w * h > 500: 
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 5)
 
-    # testing with a mask
-    #cv2.imshow('mask', mask)
-
-    # Show the final frame with the bounding boxes
+    
     cv2.imshow('frame', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
